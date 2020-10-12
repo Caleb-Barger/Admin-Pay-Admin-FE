8a48c34254f82d24ddfef387499d649b3784ba38
Caleb <bargercj20@hotmail.com>
2020-10-12 12:54:01 -0700

    redux store initalization and setup

---
+++ src/store/reducers/index.js
@@ -0,0 +1,6 @@
+import {combineReducers} from 'redux'
+import {myReducer as r1} from './myReducer'
+
+const reducer = combineReducers({r1})
+
+export default reducer
---
+++ src/store/reducers/myReducer.js
@@ -0,0 +1,10 @@
+const initalState = {}
+
+export const myReducer = (state=initalState, action) => {
+    switch(action.type) {
+        default:
+            return {
+                ...state
+            }
+    }
+}
