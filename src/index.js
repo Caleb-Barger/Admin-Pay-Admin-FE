99a51977904b9ac86e96ba3b5cbdc5e9dd8a718d
Caleb <bargercj20@hotmail.com>
2020-10-12 12:42:28 -0700

    boostraped application and added some antd components to deps

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import SiderDemo from './SiderDemo';
+import 'antd/dist/antd.css'; 
+
+ReactDOM.render(
+  <React.StrictMode>
+    <SiderDemo />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
