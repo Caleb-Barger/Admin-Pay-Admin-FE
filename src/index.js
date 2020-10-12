cfbdc4cd1e1279af2ed759cbab3fecec21c7dd1d
Caleb <bargercj20@hotmail.com>
2020-10-12 13:04:13 -0700

    created redux store and attached logger middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import {createStore, applyMiddleware} from 'redux'
+import logger from 'redux-logger'
+import {Provider} from 'react-redux'
+import reducer from './store/reducers'
 import ReactDOM from 'react-dom';
 import './index.css';
 import SiderDemo from './SiderDemo';
 import 'antd/dist/antd.css'; 
 
+const store = createStore(reducer, applyMiddleware(logger))
+
 ReactDOM.render(
-  <React.StrictMode>
-    <SiderDemo />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <SiderDemo />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
