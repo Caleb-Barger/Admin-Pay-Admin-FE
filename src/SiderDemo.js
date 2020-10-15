08a259ff6eeb7b843068af020fc169dd7173a27e
Caleb <bargercj20@hotmail.com>
2020-10-15 10:32:37 -0700

    added a few more items to the test arr

--- src/SiderDemo.js
+++ src/SiderDemo.js
@@ -23,7 +23,7 @@ class SiderDemo extends React.Component {
     });
   };
 
-  myArr = [1, 2, 3, 4, 5, 6]
+  myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
   render() {
     return (
