60419a8c4d54f1a64ec5f8a73d5cd8159b5d5e31
Caleb <bargercj20@hotmail.com>
2020-10-15 10:33:25 -0700

    added redux thunk to the apply middleware function in order to use the dispatch call

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createStore, applyMiddleware} from 'redux'
 import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
 import reducer from './store/reducers'
 import ReactDOM from 'react-dom';
@@ -8,7 +9,7 @@ import './index.css';
 import SiderDemo from './SiderDemo';
 import 'antd/dist/antd.css'; 
 
-const store = createStore(reducer, applyMiddleware(logger))
+const store = createStore(reducer, applyMiddleware(logger, thunk))
 
 ReactDOM.render(
   <Provider store={store}>
