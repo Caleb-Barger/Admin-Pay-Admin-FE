0309fcf87be69ef56a0396e47c213a57c3bdaf99
Caleb <bargercj20@hotmail.com>
2020-10-16 09:41:28 -0700

    fleshed out actions some more, now having the ability to fake a request to get all posts

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,6 +1,30 @@
+// #############################################################
+export const FETCH_POSTS_SUCCESS = "FETCH_POSTS_SUCCESS"
+export const FETCH_POSTS_START= "FETCH_POSTS_START"
+export const FETCH_POSTS_FAIL= "FETCH_POSTS_FAIL"
+// #############################################################
 export const ADD_ITEM_TO_COLLECTION_SUCCESS = "ADD_ITEM_TO_COLLECTION_SUCCESS"
 export const ADD_ITEM_TO_COLLECTION_START = "ADD_ITEM_TO_COLLECTION_START"
-//export const ADD_ITEM_TO_COLLECTION_FAIL = "ADD_ITEM_TO_COLLECTION_FAIL"
+export const ADD_ITEM_TO_COLLECTION_FAIL = "ADD_ITEM_TO_COLLECTION_FAIL"
+// #############################################################
+
+
+// !SHOULD FAIL & THE FAKE_FETCH_POSTS_RES ARE TEMPORARY
+// THEY NEED TO BE REPLACED ONCE THE API IS UP AND RUNNING!
+
+const FAKE_FETCH_POSTS_RES = [
+    "Caleb",
+    "Camden",
+    "Kyle",
+    "Kevin",
+    "Michelle",
+    "Trey",
+    "Jim",
+    "Suhr",
+    "Run 100"
+]
+
+const SHOULD_FAIL = false
 
 export const addItemToCollection = item => {
     return (dispatch) => {
@@ -8,15 +32,43 @@ export const addItemToCollection = item => {
             type: ADD_ITEM_TO_COLLECTION_START, payload: { error: "", isFetching: true }
         })
 
-        // this is where the axios call will go but for now no axios call
+        // this is where the axios call will go for testing use a setTimeout
+        setTimeout(() => console.log(`FAKE AXIOS CALL FOR ${ADD_ITEM_TO_COLLECTION_START}`),
+        3000)
+
         dispatch({
             type: ADD_ITEM_TO_COLLECTION_SUCCESS, 
             payload: { error: "", isFetching: false, item: item}
         })
 
-        // in the future will will have a fail state dispatch call something like
-        // dispatch({
-        //     type: ADD_ITEM_TO_COLLECTION_FAIL, payload: { error: "Something bad happened", isFetching: false}
-        // })
+        if (SHOULD_FAIL) {
+            dispatch({
+                type: ADD_ITEM_TO_COLLECTION_FAIL, payload: { error: `!ERROR ${ADD_ITEM_TO_COLLECTION_FAIL}!`, isFetching: false}
+            })       
+        }
+        
+    }
+}
+
+export const fetchPosts = () => {
+    return (dispatch) => {
+        dispatch({
+            type: FETCH_POSTS_START, payload: { error: "", isFetching: true}
+        })
+
+        // setTimeout(() => console.log(`FAKE AXIOS CALL FOR ${FETCH_POSTS_START}`),
+        // 3000)
+
+        dispatch({
+            type: FETCH_POSTS_SUCCESS,
+            payload: { error: "", isFetching: false, items: FAKE_FETCH_POSTS_RES }
+        })
+
+        if ( SHOULD_FAIL ) {
+            dispatch({
+                type: FETCH_POSTS_FAIL, payload: { error: `!ERROR ${FETCH_POSTS_FAIL}!`,
+                isFetching: false}
+            })
+        }
     }
 }
