b3f27f441ae11fb7dea1bfe87ddacd2f3120cf61
Caleb <bargercj20@hotmail.com>
2020-10-16 09:40:02 -0700

    overhaled app.js to map with redux store and render out a fake response

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React, {useState, useEffect} from 'react';
+import {connect} from 'react-redux'
+import {fetchPosts} from './store/actions'
+import { Layout, Menu, Typography } from 'antd';
+import './App.css';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  StarOutlined,
+  EllipsisOutlined,
+} from '@ant-design/icons';
+import Post from './components/Post';
+
+const { Title } = Typography;
+const { Header, Sider, Content } = Layout;
+
+const App = props => {
+
+  const [collapsed, setCollapsed] = useState(true)
+  const [loading, setLoading] = useState(false)
+
+  const toggle = () => {
+    setCollapsed(!collapsed)
+  };
+
+  // the collection is all items that hasSubscribed or something like ownend 
+  // when this component mounts dispatch an action call that returns all items
+    // in the background the action call will go through each item and add it to the collection
+    //  if it sees that hasSubscribed/isOwned JSON property
+
+    useEffect(() => {
+      fetchPosts()
+      if (props.isFetching !== loading) {
+        setLoading(props.isFetching)
+      }
+    }, [loading])
+
+    return (
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={collapsed} theme="light">
+          <div className="logo" />
+          <Menu theme="light" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" icon={<EllipsisOutlined />}>
+              Dashboard
+            </Menu.Item>
+            <Menu.Item key="2" icon={<StarOutlined />}>
+              Collection
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: "1rem"}}>
+            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+              className: 'trigger',
+              onClick: toggle,
+            })}
+            <Title level={5}>Admin Pay Admin v0.0.4</Title>
+          </Header>
+          <Content
+            className="site-layout-background"
+            style={{
+              padding: 12,
+              minHeight: 280,
+            }}
+          >
+              {props.isFetching ? props.items.map((v, i) => {
+                return (
+                  <Post key={i} desc={v} />
+                )
+              }) : <></>}
+          </Content>
+        </Layout>
+      </Layout>
+    );  
+  }
+
+const mapStateToProps = state => {
+  return {
+    collection: state.r1.collection,
+    items: state.r1.items,
+    isFetching: state.r1.isFetching,
+    error: state.r1.error
+  }
+} 
+
+export default connect(mapStateToProps, {fetchPosts})(App)
