4ae3068912db11d38334e8383beb4c6bf5d3bc97
Caleb <bargercj20@hotmail.com>
2020-10-17 07:52:23 -0700

    wrote handlers for the start and fail states of adding an item to the collection

--- src/store/reducers/myReducer.js
+++ src/store/reducers/myReducer.js
@@ -1,3 +1,4 @@
+import { act } from 'react-dom/test-utils'
 import {FETCH_POSTS_SUCCESS, FETCH_POSTS_START, FETCH_POSTS_FAIL,
         ADD_ITEM_TO_COLLECTION_SUCCESS, ADD_ITEM_TO_COLLECTION_START, ADD_ITEM_TO_COLLECTION_FAIL,
 } from '../actions'
@@ -36,6 +37,18 @@ export const myReducer = (state=initalState, action) => {
                 error: action.payload.error,
                 isFetching: action.payload.isFetching,
                 collection: state.collection.push(action.payload.item)
+            }
+        case ADD_ITEM_TO_COLLECTION_START:
+            return {
+                ...state,
+                error: action.payload.error,
+                isFetching: action.payload.isFetching
+            }
+        case ADD_ITEM_TO_COLLECTION_FAIL:
+            return {
+                ...state,
+                error: action.payload.error,
+                isFetching: action.payload.isFetching
             }
         default:
             return {
