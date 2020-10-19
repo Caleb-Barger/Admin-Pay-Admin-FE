cb7ea85dabe05497d7b5e7a2d5bf0e3cc842683d
Caleb <bargercj20@hotmail.com>
2020-10-19 08:21:42 -0700

    setup routes in app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom'
 import PrivateRoute from './utils/PrivateRoute'
 import Dashboard from './components/Dashboard'
+import { DASHBOARD, WIP } from './routes';
 
 const APP_VERSION = 'v0.0.7'
 
@@ -10,9 +11,12 @@ const App = () => {
   return (
     <>
     <Route path="/">
-      <p>Temporary Login Screen ;)</p>
+      <img src={WIP} />
     </Route>
-    <PrivateRoute path="dashboard"component={Dashboard} APP_VERSION={APP_VERSION} />
+    <PrivateRoute path={DASHBOARD} 
+      component={Dashboard} 
+      APP_VERSION={APP_VERSION} 
+    />
     </>
   )  
 }
