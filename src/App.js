e025295e80162ab6ef24106f0433d5d1ab2d1bd1
Caleb <bargercj20@hotmail.com>
2020-10-19 08:21:27 -0700

    added a routes src file for reducing magic strings

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom'
+import PrivateRoute from './utils/PrivateRoute'
 import Dashboard from './components/Dashboard'
 
-const APP_VERSION = 'v0.0.6'
+const APP_VERSION = 'v0.0.7'
 
 const App = () => {
 
   return (
-    <Route path="/login">
-      <Dashboard APP_VERSION={APP_VERSION}/>
+    <>
+    <Route path="/">
+      <p>Temporary Login Screen ;)</p>
     </Route>
+    <PrivateRoute path="dashboard"component={Dashboard} APP_VERSION={APP_VERSION} />
+    </>
   )  
 }
 
---
+++ src/routes/index.js
@@ -0,0 +1,2 @@
+export const DASHBOARD = "dashboard"
+export const WIP = "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/eeda1b80495429.5ce319b8f09ae.gif" 
