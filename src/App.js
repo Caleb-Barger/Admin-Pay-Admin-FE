e2e74b90feb9f8d7cbd46fb1c25016a8889422fc
Caleb <bargercj20@hotmail.com>
2020-10-20 07:52:36 -0700

    updated route strings and how app is rendering the dashboard

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom'
 import PrivateRoute from './utils/PrivateRoute'
 import Dashboard from './components/Dashboard'
-import { DASHBOARD, WIP } from './routes';
+import { DASHBOARD_ROUTE, WIP_SRC, WIP_ROUTE } from './routes';
 
 const APP_VERSION = 'v0.0.7'
 
@@ -10,13 +10,17 @@ const App = () => {
 
   return (
     <>
-    <Route path="/">
-      <img src={WIP} />
-    </Route>
-    <PrivateRoute path={DASHBOARD} 
+
+    {/* <PrivateRoute path={DASHBOARD} 
       component={Dashboard} 
       APP_VERSION={APP_VERSION} 
-    />
+    /> */}
+    <Route path={DASHBOARD_ROUTE}>
+      <Dashboard APP_VERSION={APP_VERSION} />
+    </Route>
+    <Route path={WIP_ROUTE}>
+      <img src={WIP_SRC} />
+    </Route>
     </>
   )  
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,2 +1,3 @@
-export const DASHBOARD = "dashboard"
-export const WIP = "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/eeda1b80495429.5ce319b8f09ae.gif" 
+export const DASHBOARD_ROUTE = "/dashboard"
+export const WIP_ROUTE = "/workinprogress"
+export const WIP_SRC = "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/eeda1b80495429.5ce319b8f09ae.gif" 
