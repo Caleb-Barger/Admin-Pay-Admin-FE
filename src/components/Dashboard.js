ace02354ebc2f623597a13a7c365eaa60ee2810a
Caleb <bargercj20@hotmail.com>
2020-10-20 08:26:21 -0700

    Dashboard now only does one things

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,25 +1,15 @@
-import React, {useState, useEffect} from 'react'
-import {Layout, Menu, Typography} from 'antd'
+import React, {useEffect} from 'react'
+import {Layout } from 'antd'
 import {
-    MenuUnfoldOutlined,
-    MenuFoldOutlined,
-    StarOutlined,
-    EllipsisOutlined,
 } from '@ant-design/icons'
 import {connect} from 'react-redux'
 import {fetchPosts} from '../store/actions'
 import '../App.css'
 import Post from './Post'
 
-const { Title } = Typography;
-const { Header, Sider, Content } = Layout;
+const {Content} = Layout
 
 const Dashboard = props => {
-    const [collapsed, setCollapsed] = useState(true)
-
-    const toggle = () => {
-        setCollapsed(!collapsed)
-    }
 
     useEffect(() => {
       props.fetchPosts()
@@ -27,39 +17,17 @@ const Dashboard = props => {
     }, [])
 
     return (
-      <Layout>
-        <Sider trigger={null} collapsible collapsed={collapsed} theme="light">
-          <div className="logo" />
-          <Menu theme="light" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1" icon={<EllipsisOutlined />}>
-              Dashboard
-            </Menu.Item>
-            <Menu.Item key="2" icon={<StarOutlined />}>
-              Collection
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: "1rem"}}>
-            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, { // TODO: fix React Strict Mode
-              className: 'trigger',
-              onClick: toggle,
-            })}
-            <Title level={5}>Admin Pay Admin { props.APP_VERSION }</Title>
-          </Header>
-          <Content
-            className="site-layout-background"
-            style={{
-              padding: 12,
-              minHeight: 280,
-            }}
-          >
-            {props.items.map((v, i) => {
-              return <Post key={i} desc={v} />
-            })}
-          </Content>
-        </Layout>
-      </Layout>
+      <Content
+        className="site-layout-background"
+        style={{
+          padding: 12,
+          minHeight: 280,
+        }}
+      >
+        {props.items.map((v, i) => {
+          return <Post key={i} desc={v} />
+        })}
+      </Content>
     );
 }
 
