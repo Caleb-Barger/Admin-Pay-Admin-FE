c4368fcd72c931b4ab129475a9d6b4bb99a99d0b
Caleb <bargercj20@hotmail.com>
2020-10-20 08:26:01 -0700

    organized routes and, starting to encapsulate some components

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { Route } from 'react-router-dom'
 import PrivateRoute from './utils/PrivateRoute'
 import Dashboard from './components/Dashboard'
 import { DASHBOARD_ROUTE, WIP_SRC, WIP_ROUTE } from './routes';
+import SiderNav from './components/SiderNav';
 
-const APP_VERSION = 'v0.0.7'
+const APP_VERSION = 'v0.0.8'
 
 const App = () => {
 
@@ -16,7 +17,8 @@ const App = () => {
       APP_VERSION={APP_VERSION} 
     /> */}
     <Route path={DASHBOARD_ROUTE}>
-      <Dashboard APP_VERSION={APP_VERSION} />
+      {/* <Dashboard APP_VERSION={APP_VERSION} /> */}
+      <SiderNav APP_VERSION={APP_VERSION} />
     </Route>
     <Route path={WIP_ROUTE}>
       <img src={WIP_SRC} />
