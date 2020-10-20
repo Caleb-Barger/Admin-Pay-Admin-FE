e49b2d83c4e497b91a43a71e3a2643664d471fa9
Caleb <bargercj20@hotmail.com>
2020-10-20 14:08:25 -0700

    actions can now dispatch a karmaCount to the main reducer, siteHeader currently does this but in the future it will get wired up to the openModel or something

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {Layout, Typography} from 'antd'
 import {
     MenuFoldOutlined,
     MenuUnfoldOutlined
 } from '@ant-design/icons'
+import { connect } from 'react-redux'
+import {setKarma} from '../store/actions'
 
-const {Title} = Typography
+const {Title, Text} = Typography
 const {Header} = Layout
 
-const APP_VERSION = "v0.1.0"
+const APP_VERSION = "v0.1.1"
 
 const SiteHeader = props => {
+
+    useEffect(() => {
+        props.setKarma(100)
+    }, []) // for now pretend you have 100 karma to spend
+
     return (
         <Header className="site-layout-background" style={{ padding: "1rem"}}>
             {React.createElement(props.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, { // TODO: fix React Strict Mode
@@ -18,8 +25,15 @@ const SiteHeader = props => {
                 onClick: props.toggle,
             })}
         <Title level={5}>Admin Pay Admin { APP_VERSION }</Title>
+        <Text>Your Karma: {props.karma}</Text>
         </Header>
     )
 }
 
-export default SiteHeader
+const mapStateToProps = state => {
+    return {
+        karma: state.r1.karmaCount
+    }
+}
+
+export default connect(mapStateToProps, {setKarma})(SiteHeader)
